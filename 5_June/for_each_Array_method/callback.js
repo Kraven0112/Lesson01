5982a22112c6437ea23dabc664d6ff3390c7a0c4
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-05 21:54:20 +0545

    lies

---
+++ 5_June/for_each_Array_method/callback.js
@@ -0,0 +1,14 @@
+const animals = ["Rabbit","Cat","Rat","Dog"]
+
+
+//arrow function
+animals.forEach((animal)=>{
+    console.log(animal)
+})
+
+console.log("----------------------------")
+
+//normal function
+animals.forEach(function(animal){
+    console.log(animal)
+})
