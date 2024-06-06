928d1e7c7a55ceee53c4ddb7886a51878fe16fac
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-06 23:44:48 +0545

    Lies

--- 6_June/Map_filter_reduce/script.js
+++ 6_June/Map_filter_reduce/script.js
@@ -2,24 +2,47 @@ const animals = ["Cat","Dog","Cow","Buffalo","Rat"]
 
 // forEach() method return undefined always
 
-const result1 = animals.forEach((animal)=>{
-    // return animal
-    console.log(animal)
-})
+// const result1 = animals.forEach((animal,index,arr)=>{
+//     // return animal
+//     console.log(`Index-${index}: ${animal}`)
+//     // console.log(arr)
+// })
 
-console.log(result1)
+// console.log(result1)
 
 
 
-console.log("--------------")
-console.log("--------------")
+// console.log("--------------")
+// console.log("--------------")
 
 
 
 //map return array
 
-const result2 = animals.map((animal)=>{
-    return animal
+// const result2 = animals.map((animal,index,arr)=>{
+//     console.log(`Index-${index}: ${animal}`)
+//     // console.log(arr)
+//     return animal.toUpperCase()
+// })
+
+// console.log(result2)
+
+
+
+//using filter() method
+// it creates another files or array to store
+const months = ["January","February","March","April","May","December"]
+
+//using month parameter
+// const filteredMonth = months.filter((month,index,array)=>{
+//     // return month.includes("M")
+//     return month.toLowerCase().includes("m" || "M")
+//     // return month.toUpperCase().includes("M" || "m")
+// })
+
+//using index parameter
+const filteredMonth = months.filter((month,index,array)=>{
+    return index <= 3
 })
 
-console.log(result2)
+console.log(filteredMonth)
