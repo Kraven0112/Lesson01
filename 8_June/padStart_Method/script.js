0e91c53a32adfe46e15c129b1950100ac291b4a8
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-08 12:41:09 +0545

    lies

---
+++ 8_June/padStart_Method/script.js
@@ -0,0 +1,7 @@
+//padStart() method
+const accountNumber = "8489".padStart(16, "X")
+console.log(accountNumber)
+
+//padEnd() method
+const panCardNumber = "3639".padEnd(16, "X")
+console.log(panCardNumber)
