76a91b4fda6897ecf2a94da8908529d9ecd036bc
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-17 23:59:07 +0545

    lies

---
+++ 17_June/keyboard_event/script.js
@@ -0,0 +1,16 @@
+const heading =  document.querySelector('h3')
+
+// window.addEventListener('keyup',(e)=>{
+//     console.log(e.key)
+//     console.log(e.code)
+// })
+
+window.addEventListener('keydown',(e)=>{
+    console.log(e.key)
+    console.log(e.code)
+})
+
+// window.addEventListener('keypress',(e)=>{
+//     console.log(e.key)
+//     console.log(e.code)
+// })
