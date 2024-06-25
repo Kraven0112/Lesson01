47cc504c27fd49463015e57f343216d09aa137e2
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-26 00:14:17 +0545

    lies

---
+++ 25_June/Popup_modal/script.js
@@ -0,0 +1,36 @@
+const openpopup = document.querySelector(".open")
+const modal =  document.querySelector('.main')
+const close = document.querySelector('.close-icon')
+const subscribeBtn = document.querySelector('.subscribeBtn')
+const newsletter = document.querySelector('.newsletter')
+const letter = document.querySelector('.letter')
+const inputField = document.querySelector('.inputField')
+const error = document.querySelector('.error')
+const back = document.querySelector('.back')
+
+openpopup.addEventListener('click',()=>{
+    modal.style.display="block"
+    close.addEventListener('click',()=>{
+        modal.style.display="none"
+    })
+})
+subscribeBtn.addEventListener('click',()=>{
+    if(inputField.value == false)
+        {
+            error.style.display="block"
+        }
+    else{
+        newsletter.style.display="flex"
+        modal.style.display="none"
+        inputField.value=""
+        openpopup.style.display="none"
+    }
+})
+
+letter.addEventListener('click',()=>{
+    alert("Thankyou for your support and subscription.")
+})
+back.addEventListener('click',()=>{
+     openpopup.style.display="block"
+     newsletter.style.display="none"
+})
