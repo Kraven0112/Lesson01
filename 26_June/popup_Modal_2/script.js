dc299ae8e20fc09f1c17b1e9a650c9c6b3c6ffaa
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-26 20:01:12 +0545

    lies

--- 26_June/popup_Modal_2/script.js
+++ 26_June/popup_Modal_2/script.js
@@ -1,6 +1,12 @@
 const openpop = document.querySelector('.open-popup')
 const modal = document.querySelector('.modal')
 const closepop = document.querySelector('.close-icon')
+const inputValue = document.querySelector('.email')
+const subscribeBtn = document.querySelector('.subscribeBtn')
+const error = document.querySelector('.error')
+const newsletter = document.querySelector('.newsletter')
+const goback = document.querySelector('.goback')
+const message = document.querySelector('.letter')
 
 openpop.addEventListener('click',()=>{
     modal.classList.add('open')
@@ -10,5 +16,30 @@ openpop.addEventListener('click',()=>{
         modal.classList.add('close')
         modal.classList.remove('open')
         openpop.style.display="block"
+        inputValue.value=""
     })
+})
+
+subscribeBtn.addEventListener('click',()=>{
+    if(inputValue.value == '')
+        {
+            error.style.display="block"
+        }
+    else{
+        error.style.display="none"
+        newsletter.classList.add('newsopen')
+        newsletter.classList.remove('newsclose')
+        modal.classList.add('close')
+        inputValue.value=""
+    }
+})
+goback.addEventListener('click',()=>{
+    newsletter.classList.add('newsclose')
+    newsletter.classList.remove('newsopen')
+    modal.classList.add('open')
+    modal.classList.remove('close')
+    inputValue.value=""
+})
+message.addEventListener('click',()=>{
+    alert("Thankyou for your subscription and support.")
 })
