4b36553db92f636758553a3cd64147e1b161284f
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-27 21:44:29 +0545

    lies

--- 27_June/Quiz_time/script.js
+++ 27_June/Quiz_time/script.js
@@ -1,7 +1,15 @@
 const startbefore = document.querySelector('.startbefore')
 const startingbeforecontinent  = document.querySelector('.startingbefore')
 const state_1 = document.querySelector('.state-1')
+const nextBtn = document.querySelector('.nextBtn')
 
+const questionsArray = [
+    'Which elemeny is used to highlight text in html ?',
+    'Which element is used to put javascript in html ?',
+    'whst is the use of append() method in javascript ?',
+    "What is the fullform of CSS ? ",
+    'What is the use of addEventListener() method ?'
+]
 
 startbefore.addEventListener('click',()=>{
     startingbeforecontinent.classList.add('none')
