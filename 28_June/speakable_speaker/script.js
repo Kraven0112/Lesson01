371256cce800d7110baba79a85702c9ab1a00ebd
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-29 16:13:03 +0545

    lies

--- 28_June/speakable_speaker/script.js
+++ 28_June/speakable_speaker/script.js
@@ -1,36 +1,28 @@
-const play = document.querySelector('#play')
-const reverse = document.querySelector('#reversebtn')
-const pause = document.querySelector('#pause')
-const nextbtn = document.querySelector('#nextbtn')
-
+const play = document.querySelector("#play")
+const reverse = document.querySelector("#reversebtn")
+const nextbtn = document.querySelector("#nextbtn")
 const music = [
-    'Bura_Haal.mp3',
-    'Jalsa.mp3',
-    'Jhaanjar.mp3',
-    'Lehnga.mp3',
-    'Chann Sitare.mp3'
+  "Bura_Haal.mp3",
+  "Jalsa.mp3",
+  "Jhaanjar.mp3",
+  "Lehnga.mp3",
+  "Chann Sitare.mp3",
 ]
 
 let num = 0
 let audio = new Audio(music[num])
 
-
-play.addEventListener('click',()=>{
-    num++
-    if(num == music.length)
-        {
-            num = 0
-        }
-   audio.play()
+play.addEventListener("click", () => {
+  audio.paused ? audio.play() : audio.pause()
 })
 
-pause.addEventListener('click',()=>{
-    audio.paused ? audio.play() : audio.pause()
+nextbtn.addEventListener("click", (event) => {
+  if (num == music.length) {
+    num = 0
+  }
+  audio = new Audio(music[num])
+  num++
+  audio.play()
+  event.stopPropagation()
 })
-
-nextbtn.addEventListener('click',()=>{
-   audio = new Audio(music[num++])
-   audio.play()
-})
-
 
